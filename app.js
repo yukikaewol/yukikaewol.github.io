fb30f96dca2774e02588ce47db645e25efd9969e
ttury <lovekj96@naver.com>
2021-03-26 00:33:08 +0900

    automation app.js

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-let audio = new Audio("./law1.m4a");
+
+let audio_src = document.getElementById("audio").getAttribute("src");
+let audio = new Audio(audio_src);
 
 let isClickedOnce = false;
 function click_play() {
-    if(isClickedOnce) {
+    if (isClickedOnce) {
         return null;
     }
     audio.play();
     isClickedOnce = true;
 }
-
-audio.addEventListener("ended", function() {
+audio.addEventListener("ended", function () {
     window.open("about:blank", "_self").close();
 });
